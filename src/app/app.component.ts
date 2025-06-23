ee3e2a71d66c41fa5311e7ee89c68016ca27820e
bentanic-0 <bentanico@gmail.com>
2025-06-23 17:51:46 -0300

    feat: restructure app component layout with container styling for improved UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CategorySelectorComponent } from './features/trivia/category-selector/category-selector.component';
 import { AppHeaderComponent } from './shared/components/app-header/app-header.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CategorySelectorComponent, AppHeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [
+    AppHeaderComponent,
+    RouterOutlet      
+  ],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  protected title = 'trivia-app';
-}
+export class AppComponent { }
