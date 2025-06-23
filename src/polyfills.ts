dbe5515def11a1721d6823852aa162af74e9d601
bentanic-0 <bentanico@gmail.com>
2025-06-23 17:50:41 -0300

    feat: add polyfills.ts for browser compatibility and include Zone JS for Angular

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Zone JS is required by Angular for change detection & testing
+import 'zone.js';  // Included with Angular; resolves to node_modules/zone.js/dist/zone.js
