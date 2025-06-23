289834ef7200f06575739f3e8095d309b9d73c28
bentanic-0 <bentanico@gmail.com>
2025-06-23 17:50:48 -0300

    feat: enhance main.ts to include HttpClientModule and RouterModule for improved application routing and HTTP capabilities

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(RouterModule.forRoot(routes))
+  ]
+})
+  .catch(err => console.error(err));
